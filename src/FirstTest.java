3a2bb2c813851842bc5f82890a9cf7d389fad4a7
testbyebeda <testbyebeda@gmail.com>
2020-06-21 16:02:17 +0300

    ДЗ 2.1
    Проверка на наличие текста "Serach Wikipedia"

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -31,8 +36,42 @@ public class FirstTest {
     }
 
     @Test
-    public void firstTest(){
-        System.out.println("Hello!");
+    public void assertTextIsExist(){
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'SKIP')]"),
+                "Не могу найти кнопку Skip",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Не могу найти поле для ввода запроса",
+                5
+        );
+        WebElement search_line = waitForElementPresent(
+           By.xpath("//*[@class='android.widget.EditText']"),
+           "Не найдена строка поиска",
+           15
+        );
+
+        String search_line_text = search_line.getText();
+        Assert.assertEquals(
+                "Текст не совпадает",
+                "Search Wikipedia",
+                search_line_text);
+    }
+
+    private WebElement waitForElementAndClick(By by, String error, long timeoutInSeconds){
+        WebElement element = waitForElementPresent(by, error, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutSec){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutSec);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
     }
 
 }
