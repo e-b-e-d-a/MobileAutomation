45f3bdb0b2f5996c8c0cddc08c9c4dce8fcd1f1a
testbyebeda <testbyebeda@gmail.com>
2020-07-07 21:42:58 +0300

    Update FirstTest.java
    ДЗ 5

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -11,10 +11,15 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.util.List;
+import java.util.Random;
 
 import static org.openqa.selenium.By.xpath;
 
 public class FirstTest {
+
+    //emulator @and80
+    //uiautomatorviewer
 
     private AppiumDriver driver;
 
@@ -38,45 +43,156 @@ public class FirstTest {
     }
 
     @Test
-    public void findCollectionsArticle() throws InterruptedException {
+    public void saveArticles() {
         waitForElementAndClick(
                 xpath("//*[contains(@text,'SKIP')]"),
                 "Не могу найти кнопку Skip",
                 5
         );
+        String search_field = "Search Wikipedia";
         waitForElementAndClick(
-                xpath("//*[contains(@text,'Search Wikipedia')]"),
-                "Не могу найти поле для ввода запроса",
+                xpath("//*[contains(@text,'" + search_field + "')]"),
+                "Не могу найти поле для ввода запроса" + search_field,
                 5
         );
+        String search_word = "Doom Patrol";
         waitForElementAndSendKeys(
-                xpath("//*[contains(@text,'Search Wikipedia')]"),
-                "Doom Patrol",
-                "Cannot find input",
+                xpath("//*[contains(@text,'" + search_field + "')]"),
+                search_word,
+                "Cannot find input" + search_field,
                 5
         );
-        WebElement element = waitForElementPresent(
-                xpath( "//*[@index='1']"),
-                "Не найдено больше одного результата",
+        waitForElementAndClick(
+                xpath( "//*[@class='android.widget.TextView'][@text='" + search_word +"']"),
+                "Не найдено ни одного результата",
                 15
         );
-        waitForElementAndClear(
-                xpath("//*[contains(@text,'Doom Patrol')]"),
-                "Строка поиска не очищена",
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc='Add this article to a reading list']"),
+                "Cannot find button for saving the article",
                 5
         );
-        WebElement add_language_button = waitForElementPresent(
-                By.xpath("//*[@resource-id='org.wikipedia:id/add_languages_button']"),
-                "Cannot find add button",
-                15
+        waitForElementAndClick(
+                By.xpath("//*[@text='GOT IT']"),
+                "Cannot Click on GOT IT button",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/create_button']"),
+                "Cannot click on Create new list button",
+                5
+        );
+        String my_list = "Something";
+        waitForElementAndSendKeys(
+                By.xpath("//*[@resource-id='org.wikipedia:id/text_input']"),
+                my_list,
+                "Cannot click on Create new list button",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'OK')]"),
+                "Cannot click on OK button",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot find back button",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//android.view.ViewGroup[@index='1']"),
+                "Cannot find article with index 1",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc='Add this article to a reading list']"),
+                "Cannot find button for saving the article",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='" + my_list + "']"),
+                "Cannot find " + my_list + " link",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_toolbar_button_show_overflow_menu']"),
+                "Cannot click on menu button",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_action_overflow_reading_lists']"),
+                "Cannot find My list link on menu",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='" + my_list + "']"),
+                "Cannot find " + my_list + " button",
+                5
         );
 
-        String add_button_text = add_language_button.getText();
+        int article = 2;
+        waitForElementAndClick(
+                By.xpath("//*[@class='android.view.ViewGroup'][@index='" + article + "']"),
+                "Cannot click on article with index" + article,
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc='Add this article to a reading list']"),
+                "Cannot find button for deleting the article",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='Remove from " + my_list + "']"),
+                "Cannot remove article",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_toolbar_button_show_overflow_menu']"),
+                "Cannot click on menu button",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_action_overflow_reading_lists']"),
+                "Cannot find My list link on menu",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@text='" + my_list + "']"),
+                "Cannot find " + my_list + " button",
+                5
+        );
+        String count_articles = "//*[@resource-id='org.wikipedia:id/page_list_item_title']";
+        waitForElementPresent(
+                By.xpath(count_articles),
+                "Founded more than 1 element",
+                5
+        );
+        int amountOfSearchResults = getAmountOfElements(
+                By.xpath(count_articles)
+        );
+        System.out.println(amountOfSearchResults);
+        Assert.assertTrue(
+                "We found to few results",
+                amountOfSearchResults == 1
+        );
+
+        String title = waitForElementAndGetAttribute(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title']"),
+                "text",
+                "Cannot find title of article",
+                15
+        );
+        String title_after_open_article = waitForElementAndGetAttribute(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title']"),
+                "text",
+                "Cannot find title of article",
+                15
+        );
 
         Assert.assertEquals(
-                "Не найден текст",
-                "ADD WIKIPEDIA LANGUAGES",
-                add_button_text
+                "Article title have been changed",
+                title,
+                title_after_open_article
         );
     }
 
@@ -104,6 +220,17 @@ public class FirstTest {
         WebElement element = waitForElementPresent(by, error, timeoutInSeconds);
         element.sendKeys(value);
         return element;
+    }
+
+    private int getAmountOfElements(By by){
+        List elements = driver.findElements(by);
+        return elements.size();
+    }
+
+    private String waitForElementAndGetAttribute(By by,String attribute,String error_message,long timeout)
+    {
+        WebElement element = waitForElementPresent(by,error_message,timeout);
+        return element.getAttribute(attribute);
     }
 
 }
